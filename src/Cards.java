3786092f2cda539b3c2b1b9111190a7ff5586d3e
Leirual <forgotten_ancient@wp.pl>
2016-03-01 16:02:52 +0100

    Cards v1

--- src/Cards.java
+++ src/Cards.java
@@ -1,4 +1,19 @@
 
 public class Cards {
+	
+	private String type;
+	private String typeCost;
+	private int cost;
+	
+	
+	public Cards(){
+		
+	}
+	
+	public Cards(String type, String typeCost, int cost){
+		this.type = type;
+		this.typeCost = typeCost;
+		this.cost = cost;
+	}
 
 }
